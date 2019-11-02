91554cca2c7f5d4821c6cb1e2d5fba3d6dcbf238
arnoldmak12 <arnoldmakarov@yahoo.com>
2019-11-02 12:29:47 -0400

    Created default build

---
+++ my-app/build/precache-manifest.669e86f38a5f5b50bb8483f4057be084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858d055637d3854d8938b86040b1125a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf64a4cbaf1181a78832",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "cf64a4cbaf1181a78832",
+    "url": "/static/js/main.b39afb73.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
