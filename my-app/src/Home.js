ce5a1697cb23572d9c07397b87f45c2cfda5d362
arnoldmak12 <arnoldmakarov@yahoo.com>
2019-11-02 17:24:53 -0400

    Refractored Options

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './Home.css';
 import Particles from 'react-particles-js';
 import Chess from 'chess.js';
+import Options from './Options';
 
 var select1 = ['option 1', 'option 2', 'option 3'];
 var select2 = ['option 4', 'option 5', 'option 6'];
@@ -36,57 +37,9 @@ class Home extends React.Component {
           />
         </div>
 
-        <div className="welcome-box">
-          <span className="welcome-header">
-            Welcome to Bingus Inc. Chess Engine
-          </span>
-        </div>
+      <Options />
 
-        <div className="select-box">
-          <span className="select-header">
-            Select Opponent
-          </span>
-        </div>
-
-        <div className="difficulty-box">
-
-          <ul className="difficulty-list1">
-
-            {select1.map((entry) => {
-              return (
-                <li className="difficulty-entry">
-
-                  <button className="difficulty-name">
-
-                    <span>{entry}</span>
-
-                  </button>
-
-                </li>
-              )
-            })}
-
-          </ul>
-
-          <ul className="difficulty-list2">
-
-            {select2.map((entry) => {
-              return (
-                <li className="difficulty-entry">
-
-                  <button className="difficulty-name">
-
-                    <span>{entry}</span>
-
-                  </button>
-
-                </li>
-              )
-            })}
-
-          </ul>
-
-        </div>
+        
 
       </div>
     );
---
+++ my-app/src/Options.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Options.css';
+import Particles from 'react-particles-js';
+import Chess from 'chess.js';
+
+var select1 = ['option 1', 'option 2', 'option 3'];
+var select2 = ['option 4', 'option 5', 'option 6'];
+
+class Options extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Options-layout">
+
+        <div className="welcome-box">
+          <span className="welcome-header">
+            Welcome to Bingus Inc. Chess Engine
+          </span>
+        </div>
+
+        <div className="select-box">
+          <span className="select-header">
+            Select Opponent
+          </span>
+        </div>
+
+        <div className="difficulty-box">
+
+          <ul className="difficulty-list1">
+
+            {select1.map((entry) => {
+              return (
+                <li className="difficulty-entry">
+
+                  <button className="difficulty-name"
+                    onClick={()=> window.open("someLink", "_blank")}
+                  >
+
+                    <span>{entry}</span>
+
+                  </button>
+
+                </li>
+              )
+            })}
+
+          </ul>
+
+          <ul className="difficulty-list2">
+
+            {select2.map((entry) => {
+              return (
+                <li className="difficulty-entry">
+
+                  <button className="difficulty-name">
+
+                    <span>{entry}</span>
+
+                  </button>
+
+                </li>
+              )
+            })}
+
+          </ul>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Options;
