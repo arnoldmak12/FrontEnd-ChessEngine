d53d84a1cc61f0871fc931d7a2564ccd1b4acfb6
arnoldmak12 <arnoldmakarov@yahoo.com>
2019-11-02 19:20:47 -0400

    fixed second option row for button

--- my-app/src/Options.js
+++ my-app/src/Options.js
@@ -8,76 +8,84 @@ var select1 = ['option 1', 'option 2', 'option 3'];
 var select2 = ['option 4', 'option 5', 'option 6'];
 
 class Options extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="Options-layout">
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="Options-layout">
 
-        <div className="welcome-box">
-          <span className="welcome-header">
-            Bingus Inc. Chess Engine
+                <div className="welcome-box">
+                    <span className="welcome-header">
+                        Bingus Inc. Chess Engine
           </span>
-        </div>
+                </div>
 
-        <div className="select-box">
-          <span className="select-header">
-            Select Opponent
+                <div className="select-box">
+                    <span className="select-header">
+                        Select Opponent
           </span>
-        </div>
+                </div>
 
-        <div className="difficulty-box">
+                <div className="difficulty-box">
 
-          <ul className="difficulty-list1">
+                    <ul className="difficulty-list1">
 
-            {select1.map((entry, index) => {
-              return (
-                <li className="difficulty-entry">
+                        {select1.map((entry, index) => {
+                            return (
+                                <li className="difficulty-entry">
 
-                  <button className="difficulty-name"
-                    entry = {entry.trim()}
-                    index = {index++}
-                    onClick={() => {
-                        var url = "/play/option" + index;
-                        console.log(index);
-                        window.open(url, "_self")
-                    }}
-                  >
+                                    <button className="difficulty-name"
+                                        entry={entry.trim()}
+                                        index={index++}
+                                        onClick={() => {
+                                            var url = "/play/option" + index;
+                                            console.log(index);
+                                            window.open(url, "_self")
+                                        }}
+                                    >
 
-                    <span>{entry}</span>
+                                        <span>{entry}</span>
 
-                  </button>
+                                    </button>
 
-                </li>
-              )
-            })}
+                                </li>
+                            )
+                        })}
 
-          </ul>
+                    </ul>
 
-          <ul className="difficulty-list2">
+                    <ul className="difficulty-list2">
 
-            {select2.map((entry) => {
-              return (
-                <li className="difficulty-entry">
+                        {select2.map((entry, index) => {
+                            return (
+                                <li className="difficulty-entry">
 
-                  <button className="difficulty-name">
+                                    <button className="difficulty-name"
+                                        entry={entry.trim()}
+                                        index={index++}
+                                        onClick={() => {
+                                            var url = "/play/option" + (index+3);
+                                            console.log(index);
+                                            window.open(url, "_self")
+                                        }}
+                                    >
 
-                    <span>{entry}</span>
+                                        <span>{entry}</span>
 
-                  </button>
+                                    </button>
 
-                </li>
-              )
-            })}
+                                </li>
+                            )
+                        })}
 
-          </ul>
+                    </ul>
 
-        </div>
+                </div>
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Options;
