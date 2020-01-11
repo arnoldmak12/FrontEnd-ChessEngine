3ed98bb0e4a6318f1349e578d971d536bf9305de
arnoldmak12 <arnoldmakarov@yahoo.com>
2020-01-11 16:46:54 -0500

    Communicates Properly

--- my-app/src/Gameboard.js
+++ my-app/src/Gameboard.js
@@ -14,75 +14,65 @@ class Gameboard extends React.Component {
 
   state = {
     fen: "start",
-    // array of past game moves
     history: [],
   };
 
   componentDidMount() {
-    this.game = new Chess();
-
-    this.setState({ fen: game.fen() });
+    this.setState({ 
+      fen: game.fen()
+     });
   }
 
   onDrop = ({ sourceSquare, targetSquare }) => {
     // see if the move is legal
-    let move = this.game.move({
+    let move = game.move({
       from: sourceSquare,
       to: targetSquare,
       promotion: "q"
     });
-    let postContents = sourceSquare + targetSquare;
-      $.ajax({
-        type: 'POST',
-        accepts: 'application/json',
-        data: JSON.stringify(postContents),
-        contentType: 'application/json; charset-utf-8',
-        dataType: 'json',
-        success: fxnsuccessptr,
-        error: fxnerrorptr
-      }); // always promote to a queen for example simplicity
 
     // illegal move
     if (move === null) return;
 
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('POST', '127.0.0.1:8080', true);
-    // xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    // xhr.send(JSON.stringify({postContents}));
-    // xhr.onreadystatechange = function () { // Call a function when the state changes.
-    //   if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-    //     // Request finished. Do processing here.
-    //     this.game.move({
-    //       from: this.responseText.substring(0, 2),
-    //       to: this.responseText.substring(2, 4),
-    //       promotion: this.responseText.substring(4)
-    //     })
-    //   }
-    // }
-    //xhr.send(JSON.stringify({ "move": postContents }));
-    // window.onload( () => {
-    //   $.ajax({
-    //     type: 'POST',
-    //     accepts: 'application/json',
-    //     data: JSON.stringify(postContents),
-    //     contentType: 'application/json; charset-utf-8',
-    //     dataType: 'json',
-    //     success: fxnsuccessptr,
-    //     error: fxnerrorptr
-    //   });
-    // });
+    this.setState({
+      fen: game.fen()
+    });
 
-    function fxnsuccessptr(data) {
-      this.setState(() => ({
-        fen: this.game.fen()
-      }));
-    }
+    let postContents = "" + sourceSquare + targetSquare;
 
-    function fxnerrorptr() {
-      alert("error");
-    }
+      console.log("White's Move: " + (postContents));
+      console.log(game.fen().replace(/ /g, "%20"));
+      $.ajax({
+        // traditional: true,
+        type: 'GET',
+        async: false,   
+        url: 'https://localhost:44338/api/test?fen=' + game.fen(),
+        //contentType: 'application/json',
+        dataType: 'text',
+        // data: String(postContents),
+        success: function (data) {
+          console.log("FEN Passed to API: "+ 'https://localhost:44338/api/test?fen=' + game.fen());
+          console.log("Black's Move: " + data);
+          game.move({
+             to: String(data).substring(2,4),
+             from: String(data).substring(0,2),
+             promotion: "q"
+           });
+           console.log("New FEN: "+ game.fen());
+        },
+        // error: fxnerrorptr
+        error: function (jqXHR, error, errorThrown) {
+          alert(jqXHR.responseText
+          +"\n" + error
+          +"\n" + errorThrown);
+        }
+      }); // always promote to a queen for example simplicity
+
+    this.setState({
+      fen: game.fen()
+    });
   }
-  
+
 
 
   render() {
