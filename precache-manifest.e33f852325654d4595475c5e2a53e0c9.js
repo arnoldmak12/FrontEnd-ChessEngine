b5ff06431dd5eda862d61448733a96e7477b92a2
Arnold Makarov <arnoldmakarov@yahoo.com>
2020-08-13 16:06:57 -0400

    Updates

---
+++ precache-manifest.e33f852325654d4595475c5e2a53e0c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c769a1768be7734bf8d887ef10a5a09",
+    "url": "/FrontEnd-ChessEngine/static/media/gotham-medium.1c769a17.woff"
+  },
+  {
+    "revision": "d35936d5d14cd93badf03f1893833053",
+    "url": "/FrontEnd-ChessEngine/static/media/gotham-book.d35936d5.woff"
+  },
+  {
+    "revision": "c3b32d8bbb3f9eeec1eb",
+    "url": "/FrontEnd-ChessEngine/static/js/runtime~main.e31a6aef.js"
+  },
+  {
+    "revision": "932d062ca547b61ba2fc",
+    "url": "/FrontEnd-ChessEngine/static/js/main.6ad4314f.chunk.js"
+  },
+  {
+    "revision": "abfdd7019941993e7a5a",
+    "url": "/FrontEnd-ChessEngine/static/js/2.1c856f44.chunk.js"
+  },
+  {
+    "revision": "932d062ca547b61ba2fc",
+    "url": "/FrontEnd-ChessEngine/static/css/main.ac59f98b.chunk.css"
+  },
+  {
+    "revision": "46c4eade626037dc4bccdae224088e7e",
+    "url": "/FrontEnd-ChessEngine/index.html"
+  }
+];
