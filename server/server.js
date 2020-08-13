25ef1827b0254cbcbf3beb59b62d92899cadb658
Arnold Makarov <arnoldmakarov@yahoo.com>
2020-08-13 16:57:23 -0400

    Add more Heroku Stuff

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
 
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('build'));
+   app.get('*', (req, res) => {
+     res.sendFile(path.join('build', 'index.html'));
+   });
+ }
